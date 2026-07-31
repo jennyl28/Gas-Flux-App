45679a87e32e8ac899ea7471dd25403ca625caf2
jennyl28 <jenny.laird@npl.co.uk>
2026-07-31 12:26:41 +0100

    Update gas_fluxes_dashboard4_0.py

--- gas_fluxes_dashboard4_0.py
+++ gas_fluxes_dashboard4_0.py
@@ -31,7 +31,10 @@ def clean_detection_limits(value):
 
 
 def convert_numeric(df):
-    return df.map(clean_detection_limits).apply(pd.to_numeric, errors="ignore")
+    df.map(clean_detection_limits)
+    for col in df.columns:
+        df[col] = pd.to_numeric(df[col], errors="ignore")
+    return df
 
 
 def extract_tables(file):
